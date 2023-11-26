e1b8b5faa4cccbbc1e3256f23eaeb47dbf3e6426
Leonardo Amaral da Silva Oliveira <leeonardoolliveira1@gmail.com>
2023-11-26 00:23:39 -0300

    comite3

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express")
+const exphbs = require("express-handlebars")
 
 const app = express()
+
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebrs')
+
 
 app.get('/', (requisicao, resposta) => {
     resposta.send("Olá, mundo!")
